04c5b12fef876e615a5aa74708dbf28558a958a4
Daisuke Suzuki <bboydaisuke@gmail.com>
2017-10-14 19:04:52 +0900

    Initial commit

---
+++ Assets/UnityChan/Scripts/SceneLoader.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	void OnGUI()
+	{
+		GUI.Box(new Rect(10 , Screen.height - 100 ,100 ,90), "Change Scene");
+		if(GUI.Button( new Rect(20 , Screen.height - 70 ,80, 20), "Next"))
+			LoadNextScene();
+		if(GUI.Button(new Rect(20 ,  Screen.height - 40 ,80, 20), "Back"))
+			LoadPreScene();
+	}
+
+	void LoadPreScene()
+	{
+        //int nextLevel = Application.loadedLevel + 1;    // obsolete
+        int nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextLevel <= 1)
+        {
+            //nextLevel = Application.levelCount;  // obsolete
+            nextLevel = SceneManager.sceneCountInBuildSettings;
+        }
+
+        //Application.LoadLevel(nextLevel); // obsolete
+        SceneManager.LoadScene(nextLevel);
+    }
+
+	void LoadNextScene()
+	{
+		//int nextLevel = Application.loadedLevel + 1;  // obsolete
+        int nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
+        //if (nextLevel >= Application.levelCount)  // obsolete
+        if (nextLevel >= SceneManager.sceneCountInBuildSettings)
+        {
+            nextLevel = 1;
+        }
+
+        //Application.LoadLevel(nextLevel); // obsolete
+        SceneManager.LoadScene(nextLevel);
+    }
+}
