5e3bfb1773e532132199f4d981895462801d768a
D. Suzuki <bboydaisuke@gmail.com>
2020-11-10 15:49:46 +0900

    Solve exercise

---
+++ Assets/ItemSweeper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ItemSweeper : MonoBehaviour
+{
+    void Update()
+    {
+        if (this.transform.position.z < Camera.main.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -30,6 +30,7 @@ public class UnityChanController : MonoBehaviour
     private bool isLButtonDown = false;
     //右ボタン押下の判定（追加）
     private bool isRButtonDown = false;
+    [SerializeField] bool m_godMode;
 
     // Use this for initialization
     void Start()
@@ -95,9 +96,12 @@ public class UnityChanController : MonoBehaviour
         //障害物に衝突した場合（追加）
         if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag")
         {
-            this.isEnd = true;
-            //stateTextにGAME OVERを表示（追加）
-            this.stateText.GetComponent<Text>().text = "GAME OVER";
+            if (!m_godMode)
+            {
+                this.isEnd = true;
+                //stateTextにGAME OVERを表示（追加）
+                this.stateText.GetComponent<Text>().text = "GAME OVER";
+            }
         }
 
         //ゴール地点に到達した場合（追加）
